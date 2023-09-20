36d1325921e579e24590198ffe6c0c32e9d4e054
lindatarod85 <lindatarod@gmail.com>
2023-09-20 10:23:22 +0200

    added more attemnts on tests

--- js/utils/login.test.js
+++ js/utils/login.test.js
@@ -9,3 +9,26 @@ describe("doLogin", () => {
         expect.assertions(1);
     });
     });
+
+
+
+/* Tried this*/
+   /* describe('doLogin', () => {
+        it('Should fetch the API', async () => {
+          global.fetch = jest.fn(() => Promise.resolve({}));
+      
+          await doLogin();
+      
+          expect(fetchMock).toHaveBeenCalledTimes(1);
+        });
+      })*/
+
+
+
+      /* Tried this*/
+      /*describe("doLogin", () => {
+        it("should fetch the API", async () => {
+          const data = await doLogin();
+          expect(data).toHaveBeenCalled();
+        })
+      })*/
